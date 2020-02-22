70b0e7589a1c4caae7f4603b605d4badcd8d23cd
Agaogao <854357635@qq.com>
2020-02-22 18:16:30 +0800

    又水了些

---
+++ MST01_03.java
@@ -0,0 +1,58 @@
+import java.util.Stack;
+
+class MinStack {
+    int[] a;
+    int point;
+    int cap;
+    Stack<Integer> stack;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+        stack = new Stack<>();
+        cap = 100000;
+        a = new int[cap];
+        point = 0;
+       stack.add(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        if (point == a.length - 1) {
+            int[] b = new int[a.length * 2];
+            System.arraycopy(a, 0, b, 0, a.length);
+            a = b;
+        }
+        if (x <= stack.peek()) {
+
+			stack.add(x);
+        }
+
+        a[point++] = x;
+    }
+
+    public void pop() {
+		if (a[point - 1] == stack.peek()) {
+			stack.pop();
+		}
+		point--;
+
+    }
+
+    public int top() {
+        return a[point - 1];
+    }
+
+    public int getMin() {
+		return stack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
---
+++ MST02_01.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeDuplicateNodes(ListNode head) {
+        int[] a = new int[20001];
+        if(head==null)
+            return null;
+        ListNode temp = head.next;
+        ListNode pre = head;
+        a[pre.val]++;
+        while(temp!=null)
+        {
+            if(a[temp.val]!=0)
+            {
+                pre.next=temp.next;
+                temp=temp.next;
+                continue;
+            }
+            a[temp.val]++;
+            pre=pre.next;
+            temp=temp.next;
+        }
+        return  head;
+    }
+}
