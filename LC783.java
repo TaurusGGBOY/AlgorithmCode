9f57bcfc109f883c63cd5419588ec3afde88d3cc
Agaogao <854357635@qq.com>
2020-04-15 11:21:13 +0800

    提交

--- LC783.java
+++ LC783.java
@@ -1,23 +1,41 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Vector;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null)
-            return root;
-        if (root.val == p.val || root.val == q.val)
-            return root;
-        if ((root.val < p.val && root.val > q.val) || (root.val > p.val && root.val < q.val))
-            return root;
-        if (root.val > p.val && root.val > q.val)
-            return lowestCommonAncestor(root.left, p, q);
-        else
-            return lowestCommonAncestor(root.right, p, q);
+  Vector<Integer> list = new Vector<>();
+  int minNum = Integer.MAX_VALUE;
+   public int minDiffInBST(TreeNode root) {
+        bfs(root);
+    list.sort(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1-o2;
+        }
+    });
+    for(int i=0;i<list.size()-1;i++)
+    {
+      minNum = Math.min(minNum, Math.abs(list.get(i) - list.get(i+1)));
+    }
+        return minNum;
+    }
+
+    void bfs(TreeNode node)
+    {
+        if(node==null)
+            return;
+        list.add(node.val);
+        bfs(node.left);
+        bfs(node.right);
     }
 }
--- LCP2.java
+++ LCP2.java
@@ -1,5 +1,14 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-
+  public int[] fraction(int[] cont) {
+    int[] result = new int[2];
+    result[0] = cont[cont.length - 1];
+    result[1] = 1;
+    for (int i = cont.length - 2; i >= 0; i--) {
+      int temp = result[0];
+      result[0] = result[1];
+      result[1] = temp;
+      result[0] = cont[i] * result[1] + result[0];
     }
+    return result;
+  }
 }
