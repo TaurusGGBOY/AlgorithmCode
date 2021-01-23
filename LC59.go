b63d9dc533e2193a25a8adfa3418203ba6c32592
q854357635 <854357635@qq.com>
2021-01-23 11:44:41 +0800

    补做了一题

--- LC59.go
+++ LC59.go
@@ -1,35 +1,37 @@
-
 package main
 
-func generateMatrix(n int) [][]int {
-	matrix := make([][]int,n)
-	for index := range matrix {
-		matrix[index] = make([]int,n)
+func makeConnected(n int, connections [][]int) int {
+	// 连通分量数量
+	setCount := n
+	// 冗余边数量
+	remindEdges := 0
+	parents := make([]int, n)
+	for i := range parents {
+		parents[i] = i
 	}
-	states := [][]int{{0,1},{1,0},{0,-1},{-1,0}}
-	start_pos := []int{0,0}
-	len := n
-	count:=1
-	curr_pos := []int{0,0}
-	for len>0{
-		curr_pos[0]=start_pos[0]
-		curr_pos[1]=start_pos[1]
-		if len==1{
-			matrix[curr_pos[0]][curr_pos[1]]=count
-			break
+	for _, connection := range connections {
+		src := find(parents, connection[0])
+		dst := find(parents, connection[1])
+		if src != dst {
+			merge(parents, src, dst)
+			setCount--
+		}else{
+			remindEdges++
 		}
-		for i:=0;i<(len-1)*4;i++ {
-			// len=3 i=0 state=0 i=1  state=0 i=2 state=1 i=3
-			//fmt.Printf("len=%d,i=%d,curr_pos=%d,%d\n",len,i,curr_pos[0],curr_pos[1])
-			matrix[curr_pos[0]][curr_pos[1]]=i+count
-			state := states[i/(len-1)]
-			curr_pos[0]+=state[0]
-			curr_pos[1]+=state[1]
-		}
-		count+=4*(len-1)
-		start_pos[0]++
-		start_pos[1]++
-		len-=2
 	}
-	return matrix
+	if remindEdges < setCount-1 {
+		return -1
+	}
+	return setCount - 1
+}
+
+func find(parents []int, index int) int {
+	if index != parents[index] {
+		return find(parents, parents[index])
+	}
+	return index
+}
+
+func merge(parents []int, src int, dst int) {
+	parents[dst] = src
 }
