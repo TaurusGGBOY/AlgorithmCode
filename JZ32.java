23f33933b5c705c33959786811ad951aa0997028
lab <854357635@qq.com>
2021-04-15 19:04:20 +0800

    层序遍历

---
+++ JZ32.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return new int[0];
+        }
+        queue.add(root);
+        List<Integer> list = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                list.add(node.val);
+            }
+        }
+        return list.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
